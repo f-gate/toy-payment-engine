7f7ad9cf15affe5538ca253aca832b350e39f859
f-gate <felixgate13@gmail.com>
2024-09-26 21:29:05 +0100

    field and type publicity, more derive macros

--- src/state.rs
+++ src/state.rs
@@ -1,92 +1,92 @@
+use serde::Deserialize;
 
-type ClientId = u16;
-type TxId = u32;
-type Balance = f32;
+pub type ClientId = u16;
+pub type TxId = u32;
+pub type Balance = f32;
 
 #[derive(Debug, Clone, PartialEq)]
-enum TransactionCommand {
+pub enum TransactionCommand {
     Deposit(Deposit),
     Withdrawal(Withdrawal),
     Dispute(Dispute),
     Resolve(Resolve),
-    ChargeBack(ChargeBack),
+    Chargeback(Chargeback),
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct Account {
+pub struct Account {
     /// The total funds that are available for trading, staking, withdrawal, etc. This should be equal to the total - held amounts.
-    available: Balance,
+    pub available: Balance,
     /// The total funds that are held for dispute. This should be equal to total - available amounts.
-    held: Balance,
+    pub held: Balance,
     /// The total funds that are available or held. This should be equal to available + held.
-    total: Balance,
+    pub total: Balance,
     /// Whether the account is locked. An account is locked if a charge back occurs
-    locked: Option<Locked>
+    pub locked: Option<Locked>,
 }
 
-#[derive(Debug, Clone, PartialEq)]
-struct AnyTransaction {
+#[derive(Debug, Clone, PartialEq, Deserialize)]
+pub struct AnyTransaction {
     #[serde(rename = "type")]
-    command_type: CommandType,
+    pub command_type: CommandType,
     #[serde(rename = "tx")]
-    tx_id: TxId,
+    pub tx_id: TxId,
     #[serde(rename = "client")]
-    client_id: ClientId,
-    amount: Option<Balance>
+    pub client_id: ClientId,
+    pub amount: Option<Balance>,
 }
 
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "lowercase")]  
-enum CommandType {
+#[derive(Debug, Clone, Deserialize, Default, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum CommandType {
     Deposit,
     Withdrawal,
     Dispute,
     Resolve,
     Chargeback,
     #[default]
-    Unknown, 
+    Unknown,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct Deposit {
-    client_id: ClientId,
-    tx_id: TxId,
-    amount: Balance,
+pub struct Deposit {
+    pub client_id: ClientId,
+    pub tx_id: TxId,
+    pub amount: Balance,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct Withdrawal {
-    client_id: ClientId,
-    tx_id: TxId,
-    amount: Balance,
+pub struct Withdrawal {
+    pub client_id: ClientId,
+    pub tx_id: TxId,
+    pub amount: Balance,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct Dispute {
-    client_id: ClientId,
-    tx_id: TxId,
+pub struct Dispute {
+    pub client_id: ClientId,
+    pub tx_id: TxId,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct Resolve {
-    client_id: ClientId,
-    tx_id: TxId,
+pub struct Resolve {
+    pub client_id: ClientId,
+    pub tx_id: TxId,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct Chargeback {
-    client_id: ClientId,
-    tx_id: TxId,
+pub struct Chargeback {
+    pub client_id: ClientId,
+    pub tx_id: TxId,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct Locked {
-    locked_at: u32,
-    reason_for_lock: LockReason,
+pub struct Locked {
+    pub locked_at: u32,
+    pub reason_for_lock: LockReason,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-enum LockReason {
-    ChargeBack
+pub enum LockReason {
+    Chargeback,
 }
-
