ba8691d9d4036dc72b2a15f466f07ee06094234f
f-gate <felixgate13@gmail.com>
2024-09-26 21:28:20 +0100

    CommandConverter impl

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,33 +1,100 @@
-
+use crate::state::*;
+use csv::Reader;
+use eyre::Result;
+use eyre::*;
+use std::fs::File;
 use std::sync::mpsc;
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::time::Duration;
-use eyre::Result;
-use state::*;
 
-use crate::command::AnyTransaction;
-
-struct Reader {
+struct CsvReader {
     tx: Sender<AnyTransaction>,
 }
 
-impl Reader {
-    pub fn new(tx: Sender<AnyTransaction>) {
-        Self {tx}
+impl CsvReader {
+    pub fn new(tx: Sender<AnyTransaction>) -> Self {
+        Self { tx }
     }
 
-    pub fn start(self, file_name: &str, thread_count: u8) ->  Result<()> {
+    // Todo log and ignore erroneous lines.
+    pub fn start(self, file_name: String, thread_count: u8) -> Result<()> {
         thread::spawn(move || {
-            let file = File::open(file_name).map_err(eyre!(e))?;
+            let file = File::open(file_name).unwrap();
             let mut rdr = Reader::from_reader(file);
-    
-            for result in rdr.deserialize() {
-                let tx: AnyTransaction = result.map_err(eyre!(e));
-                self.tx.send(tx)?;
-            }
 
-            Ok(())
-        })
+            for result in rdr.deserialize() {
+                let tx: AnyTransaction = result.unwrap();
+                self.tx.send(tx).unwrap();
+            }
+        });
+
+        Ok(())
+    }
+}
+
+struct CommandConverter {
+    tx: Sender<TransactionCommand>,
+    rx: Receiver<AnyTransaction>,
+}
+
+impl CommandConverter {
+    pub fn new(rx: Receiver<AnyTransaction>, tx: Sender<TransactionCommand>) -> Self {
+        Self { tx, rx }
+    }
+
+    pub fn start(self) {
+        thread::spawn(move || loop {
+            match self.rx.recv() {
+                Result::Ok(tx) => {
+                    let maybe_tx_command = match tx.command_type {
+                        CommandType::Deposit => {
+                            if let Some(amount) = tx.amount {
+                                Ok(TransactionCommand::Deposit(Deposit {
+                                    client_id: tx.client_id,
+                                    tx_id: tx.tx_id,
+                                    amount,
+                                }))
+                            } else {
+                                Err(eyre!("found erronious deposit tx, ignoring: {:?}", tx))
+                            }
+                        }
+                        CommandType::Withdrawal => {
+                            if let Some(amount) = tx.amount {
+                                Ok(TransactionCommand::Withdrawal(Withdrawal {
+                                    client_id: tx.client_id,
+                                    tx_id: tx.tx_id,
+                                    amount,
+                                }))
+                            } else {
+                                Err(eyre!("found erronious withdrawal tx, ignoring: {:?}", tx))
+                            }
+                        }
+                        CommandType::Dispute => Ok(TransactionCommand::Dispute(Dispute {
+                            client_id: tx.client_id,
+                            tx_id: tx.tx_id,
+                        })),
+                        CommandType::Resolve => Ok(TransactionCommand::Resolve(Resolve {
+                            client_id: tx.client_id,
+                            tx_id: tx.tx_id,
+                        })),
+                        CommandType::Chargeback => Ok(TransactionCommand::Chargeback(Chargeback {
+                            client_id: tx.client_id,
+                            tx_id: tx.tx_id,
+                        })),
+                        CommandType::Unknown => Err(eyre!("found unknown tx, ignoring: {:?}", tx)),
+                    };
+
+                    match maybe_tx_command {
+                        Result::Ok(tx_command) => self.tx.send(tx_command).unwrap(),
+                        Err(e) => tracing::error!(
+                            "Failed to convert AnyTransaction into TransactionCommand:\n{:?}",
+                            e
+                        ),
+                    }
+                }
+                Err(e) => todo!("Handle error"),
+            }
+        });
     }
 }
